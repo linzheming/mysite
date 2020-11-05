6b7972913e15d8f0649019fa4491d9a231cc14cd
林哲明 <zhezhelin618@gmail.com>
2020-11-05 11:26:24 +0800

    修改admin页面相关标题

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from cookies.admin_cookies_my import cookie_my_admin_site
 
+admin.site.site_header = "TOP HACKER Admin"
+admin.site.site_title = "Admin Portal"
+admin.site.index_title = "Welcome to TOP HACKER Researcher Portal"
+
 urlpatterns = [
     path('cookies/', include('cookies.urls')),
     path('admin/', admin.site.urls),
